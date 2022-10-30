e076ca2dd2ba8d58ecd4556445a1ae368dd31322
Nelson Matos <nmatos6@gatech.edu>
2022-10-30 15:01:53 -0500

    Converted base application to be a command line application.

--- src/main/java/com/nm1022/tool/rental/ToolRentalApplication.java
+++ src/main/java/com/nm1022/tool/rental/ToolRentalApplication.java
@@ -1,13 +1,18 @@
 package com.nm1022.tool.rental;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ToolRentalApplication {
+public class ToolRentalApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ToolRentalApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) {
+		System.out.println("Hello World");
+	}
 }
