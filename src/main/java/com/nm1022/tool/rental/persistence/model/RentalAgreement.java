d03ab19d428288138bb899fd36b6b475ef2213c0
Nelson Matos <nmatos6@gatech.edu>
2022-11-05 16:00:50 -0500

    Implemented CheckoutService. Implemented RentalAgreement builder.

--- src/main/java/com/nm1022/tool/rental/persistence/model/RentalAgreement.java
+++ src/main/java/com/nm1022/tool/rental/persistence/model/RentalAgreement.java
@@ -1,20 +1,71 @@
 package com.nm1022.tool.rental.persistence.model;
 
+import com.nm1022.tool.rental.util.CalendarUtil;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.LocalDate;
+import java.util.List;
 
 @Getter
+@Setter
 public class RentalAgreement {
     private Tool tool;
-    private short rentalDays;
-    private LocalDateTime checkoutDate;
-    private LocalDateTime dueDate;
-    private float dailyRentalCharge;
-    private short totalChargeDays;
-    private double preDiscountCharge;
+    private int rentalDays;
+    private LocalDate checkoutDate;
+    private LocalDate dueDate;
+    private int totalChargeDays;
+    private BigDecimal preDiscountCharge;
+    private BigDecimal dailyRentalCharge;
     private byte discountPercent;
-    private double discountAmount;
-    private double finalCharge;
+    private BigDecimal discountAmount;
+    private BigDecimal finalCharge;
+
+    public static class RentalAgreementBuilder {
+        private Tool tool;
+        private int rentalDays;
+        private LocalDate checkoutDate;
+        private int chargeDays;
+        private byte discountPercent;
+
+        public RentalAgreementBuilder(Tool tool, int rentalDays, LocalDate checkoutDate, int chargeDays, byte discountPercent) {
+            this.tool = tool;
+            this.rentalDays = rentalDays;
+            this.checkoutDate = checkoutDate;
+            this.chargeDays = chargeDays;
+            this.discountPercent = discountPercent;
+        }
+
+        public RentalAgreement build() {
+            RentalAgreement rentalAgreement = new RentalAgreement();
+
+            rentalAgreement.setTool(this.tool);
+            rentalAgreement.setRentalDays(this.rentalDays);
+            rentalAgreement.setCheckoutDate(this.checkoutDate);
+
+            List<LocalDate> rentalDates = CalendarUtil.getRentalDates(this.checkoutDate, this.rentalDays);
+            rentalAgreement.setDueDate(rentalDates.get(rentalDates.size()-1));
+
+            rentalAgreement.setDailyRentalCharge(this.tool.getRentalCharge().getDailyCharge());
+
+            rentalAgreement.setTotalChargeDays(this.chargeDays);
+
+            BigDecimal preDiscountCharge =
+                    this.tool.getRentalCharge().getDailyCharge()
+                            .multiply(BigDecimal.valueOf(this.chargeDays))
+                            .setScale(2, RoundingMode.HALF_UP);
+
+            rentalAgreement.setPreDiscountCharge(preDiscountCharge);
+            rentalAgreement.setDiscountPercent(this.discountPercent);
+
+            BigDecimal discountAmount = preDiscountCharge.multiply(BigDecimal.valueOf((this.discountPercent / 100.0)))
+                    .setScale(2, RoundingMode.HALF_UP);
+            rentalAgreement.setDiscountAmount(discountAmount);
+
+            rentalAgreement.setFinalCharge(preDiscountCharge.subtract(discountAmount));
+            return rentalAgreement;
+        }
+    }
 }
--- src/main/java/com/nm1022/tool/rental/service/CheckoutService.java
+++ src/main/java/com/nm1022/tool/rental/service/CheckoutService.java
@@ -1,15 +1,32 @@
 package com.nm1022.tool.rental.service;
 
+import com.nm1022.tool.rental.handler.rental.term.RentalTermProcessor;
 import com.nm1022.tool.rental.persistence.model.RentalAgreement;
-import com.nm1022.tool.rental.util.CalendarUtil;
+import com.nm1022.tool.rental.persistence.model.Tool;
+import com.nm1022.tool.rental.persistence.repository.ToolRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
-import java.util.List;
 
 @Service
 public class CheckoutService {
+
+    private final RentalTermProcessor rentalTermProcessor;
+    private final ToolRepository toolRepository;
+
+    @Autowired
+    public CheckoutService(RentalTermProcessor rentalTermProcessor, ToolRepository toolRepository) {
+        this.rentalTermProcessor = rentalTermProcessor;
+        this.toolRepository = toolRepository;
+    }
+
     public RentalAgreement checkout(String toolCode, int numOfRentalDays, byte discountPercent, LocalDate checkoutDate ) {
-        return new RentalAgreement();
+        Tool tool = toolRepository.findByCode(toolCode);
+        int chargeDays = rentalTermProcessor.process(checkoutDate, numOfRentalDays, tool.getRentalCharge().getChargeIndicators());
+
+        return new RentalAgreement
+                .RentalAgreementBuilder(tool, numOfRentalDays, checkoutDate, chargeDays, discountPercent)
+                .build();
     }
 }
